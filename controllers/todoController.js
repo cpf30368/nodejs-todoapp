e0bc90bddcbee453ba260ac2a1bf70636de74601
Paul <jamespauloconnor@gmail.com>
2016-07-29 21:50:38 -0400

    Some wrestling with EJS

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,17 +1,17 @@
+var data = [{item: 'eat sushi'}];
+
 module.exports = function(app){
 
 //Render a view when user makes request
 app.get('/todo', function(req, res){
-	res.render('todo');
+	res.render('todo', {todos: data});
 });
 
 app.post('/todo', function(req, res){
 
-
 });
 
 app.delete('/todo', function(req, res){
-
 
 });
 };
